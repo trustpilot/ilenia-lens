7b939c000b74686f30c20f265144a8edd1b2dba6
Viktor Danilov <duke.img@gmail.com>
2020-01-15 15:46:30 +0200

    Init state with workspace projects

---
+++ src/state.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export async function initIndex(context: vscode.ExtensionContext, projects: any) {
+    const index = {} as any;
+    Object.keys(projects).map((p) => {
+        index[p] = {};
+    });
+    await context.workspaceState.update('index', index);
+}
